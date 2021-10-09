54dbb7ad38da3cb1b68186dd8e91bb446182fafe
xs.Liu <liuxianshi@baozhunniu.com>
2021-10-09 14:07:37 +0800

    user表调整

--- mango-user-service/src/main/java/com/mango/user/domain/model/User.java
+++ mango-user-service/src/main/java/com/mango/user/domain/model/User.java
@@ -2,9 +2,11 @@ package com.mango.user.domain.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * 用户表
@@ -37,44 +39,83 @@ public class User implements Serializable {
     private String nickName;
 
     /**
-     * 性别
+     * 性别【1男，2女】
+     * {@link com.mango.core.enums.GenderEnum}
      */
-    private Boolean sex;
+    private Short sex;
 
     /**
-     * 删除状态
+     * 删除状态【0已删除，1未删除】
+     * {@link com.mango.core.enums.DeleteStatusEnum}
      */
     @Column(name = "delete_status")
     private Short deleteStatus;
 
     /**
-     * 电话
+     * 手机号
      */
     @Column(name = "user_phone")
     private String userPhone;
 
     /**
      * 证件类型
+     * {@link com.mango.core.enums.IdTypeEnum}
      */
     @Column(name = "user_id_card_type")
-    private Integer userIdCardType;
+    private Short userIdCardType;
 
     /**
      * 用户等级
+     * {@link com.mango.core.enums.LvEnum}
      */
     @Column(name = "user_level")
     private Short userLevel;
 
     /**
-     * 用户类型
-     */
-    @Column(name = "user_type")
-    private Integer userType;
-
-    /**
      * 出生日期
      */
-    private Date birthday;
+    private LocalDateTime birthday;
+
+    /**
+     * 邮箱
+     */
+    @Column(name = "user_email")
+    private String userEmail;
+
+    /**
+     * 密码
+     */
+    @Column(name = "user_pwd")
+    private String userPwd;
+
+    /**
+     * 盐
+     */
+    private String salt;
+
+    /**
+     * 证件号
+     */
+    @Column(name = "user_id_card")
+    private String userIdCard;
+
+    /**
+     * 证件名
+     */
+    @Column(name = "user_id_card_name")
+    private String userIdCardName;
+
+    /**
+     * 用户介绍
+     */
+    @Column(name = "user_introduce")
+    private String userIntroduce;
+
+    /**
+     * 用户头像
+     */
+    @Column(name = "user_head_img")
+    private String userHeadImg;
 
     /**
      * 状态
@@ -85,52 +126,11 @@ public class User implements Serializable {
      * 创建时间
      */
     @Column(name = "create_time")
-    private Date createTime;
+    private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
     @Column(name = "update_time")
-    private Date updateTime;
-
-    /**
-     * 邮箱
-     */
-    @Column(name = "user_email")
-    private byte[] userEmail;
-
-    /**
-     * 密码
-     */
-    @Column(name = "user_pwd")
-    private byte[] userPwd;
-
-    /**
-     * 盐
-     */
-    private byte[] salt;
-
-    /**
-     * 证件号
-     */
-    @Column(name = "user_id_card")
-    private byte[] userIdCard;
-
-    /**
-     * 证件名
-     */
-    @Column(name = "user_id_card_name")
-    private byte[] userIdCardName;
-
-    /**
-     * 用户介绍
-     */
-    @Column(name = "user_introduce")
-    private byte[] userIntroduce;
-
-    /**
-     * 用户头像
-     */
-    @Column(name = "user_head_img")
-    private byte[] userHeadImg;
+    private LocalDateTime updateTime;
 }
